364bdcbbb09db309b98fc0667b54f266612d8428
dougbradbury <doug@8thlight.com>
2011-07-29 11:47:38 -0500

    function definitions to pass strict compile

--- src/CSlim/ListExecutor.c
+++ src/CSlim/ListExecutor.c
@@ -33,7 +33,7 @@ static void AddResult(SlimList* list, char* id, char* result)
 	SlimList_Destroy(pair);	
 }
 
-char* InvalidCommand(ListExecutor* self, SlimList* instruction) {
+char* InvalidCommand(SlimList* instruction) {
 	char* id = SlimList_GetStringAt(instruction, 0);
 	char* command = SlimList_GetStringAt(instruction, 1);
 	static char msg[128];
@@ -41,13 +41,13 @@ char* InvalidCommand(ListExecutor* self, SlimList* instruction) {
 	return CSlim_BuyString(msg);
 }
 
-char* MalformedInstruction(ListExecutor* self, SlimList* instruction) {
+char* MalformedInstruction(SlimList* instruction) {
 	static char msg[128];
 	snprintf(msg, (size_t)128, "__EXCEPTION__:message:<<MALFORMED_INSTRUCTION %s.>>", SlimList_ToString(instruction));
 	return CSlim_BuyString(msg);
 }
 
-char* Import(ListExecutor* self, SlimList* instruction) {
+char* Import() {
 	return CSlim_BuyString("OK");
 }
 
@@ -64,7 +64,7 @@ char* Call(ListExecutor* self, SlimList* instruction) {
   char *instanceName, *methodName, *result;
   SlimList* args;
 	if (SlimList_GetLength(instruction) < 4)
-		return MalformedInstruction(self, instruction);
+		return MalformedInstruction(instruction);
 	instanceName = SlimList_GetStringAt(instruction, 2);
 	methodName = SlimList_GetStringAt(instruction, 3);
 	args = SlimList_GetTailAt(instruction, 4);
@@ -77,7 +77,7 @@ char* CallAndAssign(ListExecutor* self, SlimList* instruction) {
   char *symbolName, *instanceName, *methodName, *result;
   SlimList* args;
 	if (SlimList_GetLength(instruction) < 5)
-		return MalformedInstruction(self, instruction);
+		return MalformedInstruction(instruction);
 	symbolName = SlimList_GetStringAt(instruction, 2);
 	instanceName = SlimList_GetStringAt(instruction, 3);
 	methodName = SlimList_GetStringAt(instruction, 4);
@@ -91,7 +91,7 @@ char* CallAndAssign(ListExecutor* self, SlimList* instruction) {
 char* Dispatch(ListExecutor* self, SlimList* instruction) {
 	char* command = SlimList_GetStringAt(instruction, 1);
 	if (strcmp(command, "import") == 0)
-		return Import(self, instruction);
+		return Import();
 	else if (strcmp(command, "make") == 0)
 		return Make(self, instruction);
 	else if (strcmp(command, "call") == 0)
@@ -99,7 +99,7 @@ char* Dispatch(ListExecutor* self, SlimList* instruction) {
 	else if (strcmp(command, "callAndAssign") == 0)
 		return CallAndAssign(self, instruction);
 	else
-		return InvalidCommand(self, instruction);
+		return InvalidCommand(instruction);
 }
 
 SlimList* ListExecutor_Execute(ListExecutor* self, SlimList* instructions)
