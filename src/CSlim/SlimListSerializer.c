835be120b8467eaef9ed8de08abb56b576efb8bb
crapula2010 <jpross@bluebottle.com>
2012-08-02 17:39:41 +0200

    Update src/CSlim/SlimListSerializer.c
    Updated to calculate field length as number of characters when multibyte characters are in the list rather than the number of bytes

--- src/CSlim/SlimListSerializer.c
+++ src/CSlim/SlimListSerializer.c
@@ -8,39 +8,47 @@ enum {LIST_OVERHEAD=9, ELEMENT_OVERHEAD=8};
 
 char* nodeStringAt(SlimList* self, int i)
 {
-	char * nodeString = SlimList_GetStringAt(self, i);
-	if (nodeString == NULL)
-		nodeString = "null";		
-	return nodeString;
+        char * nodeString = SlimList_GetStringAt(self, i);
+        if (nodeString == NULL)
+                nodeString = "null";
+        return nodeString;
 }
 
 int SlimList_SerializedLength(SlimList* self)
 {
-	int length = LIST_OVERHEAD;
-	int i;
-	for(i = 0; i < SlimList_GetLength(self); i++)
-	{
-		length += strlen(nodeStringAt(self, i)) + ELEMENT_OVERHEAD;
-	}
-	return length;
+        int length = LIST_OVERHEAD;
+        int i;
+        for(i = 0; i < SlimList_GetLength(self); i++)
+        {
+                length += strlen(nodeStringAt(self, i)) + ELEMENT_OVERHEAD;
+        }
+        return length;
 }
 
 char* SlimList_Serialize(SlimList* self)
 {
-	char* buf = (char*)malloc(SlimList_SerializedLength(self)+1);
-	char* write_ptr = buf;
-	int listLength = SlimList_GetLength(self);
-	int i;
-		
-	write_ptr += sprintf(write_ptr, "[%06d:", listLength);
+        char* buf = (char*)malloc(SlimList_SerializedLength(self)+1);
+        char* write_ptr = buf;
+        int listLength = SlimList_GetLength(self);
+        int i;
 
-	for(i = 0; i < listLength; i++)
-	{
-		char * nodeString = nodeStringAt(self, i);
-		write_ptr += sprintf(write_ptr, "%06ld:%s:", (long)strlen(nodeString), nodeString);
-	}
-	strcpy(write_ptr, "]");
-	return buf;
+        write_ptr += sprintf(write_ptr, "[%06d:", listLength);
+
+        for(i = 0; i < listLength; i++)
+        {
+                char * nodeString = nodeStringAt(self, i);
+                // jpr strlen returns number of bytes but we want chars
+                // characters 
+                unsigned char *p;
+                long fieldlength=0;
+                for (p=nodeString;*p;p++)
+                  if ((*p < 0x80)||(*p > 0xbf))
+                   fieldlength++;
+
+                write_ptr += sprintf(write_ptr, "%06ld:%s:", fieldlength, nodeString);
+        }
+        strcpy(write_ptr, "]");
+        return buf;
 }
 
 void SlimList_Release(char *serializedResults)
