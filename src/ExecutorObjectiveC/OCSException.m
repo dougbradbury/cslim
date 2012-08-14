5b8283618e658b942eb56e8ed79d1b53a782cd33
Eric Meyer <emeyer@8thlight.com>
2012-08-14 11:50:24 -0500

    Refactored OCSException to use initWithFormattedMessage

--- src/ExecutorObjectiveC/OCSException.m
+++ src/ExecutorObjectiveC/OCSException.m
@@ -3,22 +3,25 @@
 @implementation OCSException
 
 +(id) exceptionWithNSException:(NSException*) givenException {
-    OCSException* exception = [self new];
-    exception.stringValue = [NSString stringWithFormat: @"__EXCEPTION__:message:<<%@ %@>>",
-                             [givenException name],
-                             [givenException reason]];
-    
-    return exception;
+    NSString* formattedMessage = [NSString stringWithFormat: @"%@ %@",
+                                  [givenException name],
+                                  [givenException reason]];
+    return [[self alloc] initWithFormattedMessage: formattedMessage];
 }
 
 +(id) exceptionWithMessage:(NSString*) message, ... NS_FORMAT_FUNCTION(1,2) {
     va_list args;
     va_start(args, message);
-    OCSException* exception = [self new];
     NSString *formattedMessage = [[NSString alloc] initWithFormat: message arguments:args];
-    exception.stringValue = [NSString stringWithFormat: @"__EXCEPTION__:message:<<%@>>", formattedMessage];
     va_end(args);
-    return exception;
+    return [[self alloc] initWithFormattedMessage: formattedMessage];
+}
+
+-(id) initWithFormattedMessage:(NSString*) formattedMessage {
+    if ((self = [super init])) {
+        self.stringValue = [NSString stringWithFormat: @"__EXCEPTION__:message:<<%@>>", formattedMessage];
+    }
+    return self;
 }
 
 @end
